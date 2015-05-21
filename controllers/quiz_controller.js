061b215060fd62cacd095ad6eb9e03d89d03310e
Lartsen <a.ballesterosr@alumnos.upm.es>
2015-05-21 17:49:26 +0200

    Validate y editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,8 +1,8 @@
 var models = require('../models/models.js');
 
-// Autoload - factoriza eñ código si ruta incluye :quizId
+// Autoload - factoriza el código si ruta incluye :quizId
 exports.load = function(req, res, next, quizId) {
- models.Quiz.find(quizId).then(
+ models.Quiz.findById(quizId).then(
   function(quiz) {
    if(quiz) {
     req.quiz = quiz;
@@ -16,19 +16,19 @@ exports.load = function(req, res, next, quizId) {
 exports.index = function(req, res) {
 if(req.query.search){
   models.Quiz.findAll({where: ["pregunta like ?", '%' + req.query.search + '%']}).then(function(quizes) {
-    res.render('quizes/index', { quizes: quizes});
+    res.render('quizes/index', { quizes: quizes, errors: []});
   });
 }
 else{
   models.Quiz.findAll().then(function(quizes) {
-    res.render('quizes/index', { quizes: quizes});
+    res.render('quizes/index.ejs', { quizes: quizes, errors: []});
   });
 }
  };
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-	res.render('quizes/show', {quiz: req.quiz});
+	res.render('quizes/show', {quiz: req.quiz, errors: []});
 };
 
 // GET /quizes/:id/answer
@@ -38,11 +38,55 @@ exports.answer = function(req, res) {
   if(req.query.respuesta === req.quiz.respuesta){
 	resultado = 'Correcto';
    }
-   res.render('quizes/answer', {quiz: req.quiz, respuesta:resultado});
+   res.render('quizes/answer', {quiz: req.quiz, respuesta:resultado, errors: []});
+};
+
+// GER /quizes/new
+
+exports.new = function(req,res) {
+  var quiz = models.Quiz.build(// crea objeto quiz
+    {pregunta: "Pregunta", respuesta: "Respuesta"}
+    );
+  res.render('quizes/new', {quiz: quiz, errors: []});
+};
+
+// POST /quizes/create
+
+exports.create = function(req, res) {
+ var quiz = models.Quiz.build( req.body.quiz );
+
+ quiz.validate().then(function(err){
+  if(err){res.render('quizes/new', {quiz: quiz, errors: err.errors});}
+  else{quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){//guarda en DB los campos pregunta y respuesta de quiz
+    res.redirect('/quizes')})// Redirección HTTP (URL relativo) lista de preguntas
+}
+ });
+};
+
+// GET /quizes/:id/edit
+
+exports.edit = function(req, res) {
+  var quiz = req.quiz; // autoload de instancia de quiz
+
+  res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+// PUT /quizes/:od
+
+exports.update = function(req, res) {
+ req.quiz.pregunta = req.body.quiz.pregunta;
+ req.quiz.respuesta = req.body.quiz.respuesta;
+
+ req.quiz.validate().then(function(err){
+  if(err){res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});}
+  else{req.quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){//guarda en DB los campos pregunta y respuesta de quiz
+    res.redirect('/quizes');});// Redirección HTTP (URL relativo) lista de preguntas
+}
+ });
 };
 
 // GET /authors/author
 
 exports.author = function(req, res) {
-	res.render('authors/author');
+	res.render('authors/author', {errors: []});
 };
